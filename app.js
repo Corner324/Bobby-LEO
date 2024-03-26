fced64bed291786f7473f0d70810f4a90c235559
Corner324 <mosenn@mail.ru>
2024-03-26 19:31:18 +0300

    Fix date + embed time

--- app.js
+++ app.js
@@ -381,6 +381,7 @@ app.post('/interactions', async function (req, res) {
           body: {
             embeds: [
               {
+                ...messagesData.embeds[0], // сохраняем все свойства из исходного embed
                 description: messagesData.embeds[0].description + `**Продолжительность:** ${twoDigits(patrol_time.getUTCHours())}:${twoDigits(patrol_time.getUTCMinutes())}\n\u200B`
               }
             ]
